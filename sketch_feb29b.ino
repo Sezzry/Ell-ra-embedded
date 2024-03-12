2b054294b35e1e0c4e9b39d06d17fd730794c942
Saran Kongthong <146092374+Sezzry@users.noreply.github.com>
2024-03-12 23:35:02 +0100

    Uppgift6.ino

--- sketch_feb29b.ino
+++ sketch_feb29b.ino
@@ -1,29 +1,54 @@
-const int redLedPin = 11;  
-const int greenLedPin = 9; 
-const int blueLedPin = 3;  
+const int BUTTON_PIN_7 = 2;
+const int BUTTON_PIN_13 = 3;
+const int LED_PIN_9 = 9;
+const int LED_PIN_11 = 11;
+const int POTENTIOMETER_PIN = A0;
+const int PWM_OUTPUT_PIN = 6;
+
+int ledState_9 = LOW;
+int ledState_11 = LOW;
+int potValue = 0;
+unsigned long lastDebounceTime_7 = 0;
+unsigned long lastDebounceTime_13 = 0;
+unsigned long debounceDelay = 50;
 
 void setup() {
-  pinMode(redLedPin, OUTPUT);
-  pinMode(greenLedPin, OUTPUT);
-  pinMode(blueLedPin, OUTPUT);
+  pinMode(LED_PIN_9, OUTPUT);
+  pinMode(LED_PIN_11, OUTPUT);
+  pinMode(BUTTON_PIN_7, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_13, INPUT_PULLUP);
+  pinMode(POTENTIOMETER_PIN, INPUT);
+  pinMode(PWM_OUTPUT_PIN, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN_7), buttonInterrupt_7, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN_13), buttonInterrupt_13, FALLING);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(redLedPin, HIGH);
-  delay(1000);
-  digitalWrite(redLedPin, LOW);
+  potValue = analogRead(POTENTIOMETER_PIN);
+  int pwmOutput = map(potValue, 0, 1023, 0, 255);
+  analogWrite(PWM_OUTPUT_PIN, pwmOutput);
 
-  digitalWrite(greenLedPin, HIGH);
-  delay(1000);
-  digitalWrite(greenLedPin, LOW);
+  Serial.print("Potentiometer Value: ");
+  Serial.println(potValue);
 
-  digitalWrite(blueLedPin, HIGH);
-  delay(1000); 
-  digitalWrite(blueLedPin, LOW);
-
-  delay(1000);
+  delay(100);
 }
 
-// I = V/R = 5V/220 ohm
-// I = 5/220 = 0.0227 A ungefär för varje diod
-// Strömmen är ca 22.7 mA milliampeere per diod
+void buttonInterrupt_7() {
+  if (millis() - lastDebounceTime_7 > debounceDelay) {
+    ledState_9 = !ledState_9;
+    digitalWrite(LED_PIN_9, ledState_9);
+    lastDebounceTime_7 = millis();
+  }
+}
+
+void buttonInterrupt_13() {
+  if (millis() - lastDebounceTime_13 > debounceDelay) {
+    ledState_11 = !ledState_11;
+    digitalWrite(LED_PIN_11, ledState_11);
+    lastDebounceTime_13 = millis();
+  }
+}
