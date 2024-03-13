5aaed7b1c06bab4705dc15c6ecf9645550029975
Saran Kongthong <146092374+Sezzry@users.noreply.github.com>
2024-03-13 22:59:56 +0100

    Update Uppgift3.ino

--- Uppgift3.ino
+++ Uppgift3.ino
@@ -1,55 +1,51 @@
-const int BUTTON_PIN_7 = 7;  
-const int BUTTON_PIN_13 = 13; 
-const int LED_PIN_9 = 9;     
-const int LED_PIN_11 = 11;    
-const int POTENTIOMETER_PIN = A0; 
-const int PWM_OUTPUT_PIN = 3;     
+const int buttonPin1 = 13; 
+const int buttonPin2 = 7;  
+const int potPin = A0;    
+const int ledPinR = 11;    
+const int ledPinG = 9;     
+const int ledPinB = 3;     
 
-int buttonState_7 = 0;       
-int buttonState_13 = 0;      
-int ledState_9 = LOW;        
-int ledState_13 = LOW;       
-int potValue = 0;            
+int buttonState1 = 0;      
+int buttonState2 = 0;      
+int lastButtonState1 = 0;  
+int lastButtonState2 = 0;  
+int potValue = 0;          
 
 void setup() {
-  pinMode(LED_PIN_9, OUTPUT);
-  pinMode(LED_PIN_11, OUTPUT);
-  pinMode(BUTTON_PIN_7, INPUT_PULLUP);
-  pinMode(BUTTON_PIN_13, INPUT_PULLUP);
-  pinMode(POTENTIOMETER_PIN, INPUT);
-  pinMode(PWM_OUTPUT_PIN, OUTPUT);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(potPin, INPUT);
+  pinMode(ledPinR, OUTPUT);
+  pinMode(ledPinG, OUTPUT);
+  pinMode(ledPinB, OUTPUT);
 
-  Serial.begin(9600); 
+  Serial.begin(9600);
 }
 
 void loop() {
-  buttonState_7 = digitalRead(BUTTON_PIN_7);
-  buttonState_13 = digitalRead(BUTTON_PIN_13);
+  buttonState1 = digitalRead(buttonPin1);
 
-  if (buttonState_7 == LOW) {
-    ledState_9 = (ledState_9 == LOW) ? HIGH : LOW;
-    digitalWrite(LED_PIN_9, ledState_9);
-
-    while (digitalRead(BUTTON_PIN_7) == LOW) {
-      delay(10);
+  if (buttonState1 != lastButtonState1) {
+    if (buttonState1 == LOW) {
+      digitalWrite(ledPinR, !digitalRead(ledPinR));
     }
   }
 
-  if (buttonState_13 == LOW) {
-    ledState_13 = (ledState_13 == LOW) ? HIGH : LOW;
-    digitalWrite(LED_PIN_11, ledState_13);
+  buttonState2 = digitalRead(buttonPin2);
 
-    while (digitalRead(BUTTON_PIN_13) == LOW) {
-      delay(10);
+  if (buttonState2 != lastButtonState2) {
+    if (buttonState2 == LOW) {
+      digitalWrite(ledPinG, !digitalRead(ledPinG));
     }
   }
 
-  potValue = analogRead(POTENTIOMETER_PIN);
-  int pwmOutput = map(potValue, 0, 1023, 0, 255);
+  potValue = analogRead(potPin);
+  analogWrite(ledPinB, potValue / 4); 
 
-  analogWrite(PWM_OUTPUT_PIN, pwmOutput);
-
-  Serial.print("Potentiometer Value: ");
+  Serial.print("Potentiometer value (LED-B): ");
   Serial.println(potValue);
-  delay(100); 
+  delay(200);
+
+  lastButtonState1 = buttonState1;
+  lastButtonState2 = buttonState2;
 }
