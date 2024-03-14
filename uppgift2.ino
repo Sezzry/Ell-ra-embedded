5e4214e24bde1f142eb00140cce6c76a86a77a23
Saran Kongthong <146092374+Sezzry@users.noreply.github.com>
2024-03-14 22:57:55 +0100

    Update uppgift2.ino

--- uppgift2.ino
+++ uppgift2.ino
@@ -5,9 +5,14 @@ void setup() {
 }
 
 void loop() {
+  // Läser av värdet från potentiometer 
   int sensorValue = analogRead(potPin); 
+
+  // Spänningen är 5v och arduino
+  // Formeln multiplicerar det analoga värdet med förhållandet mellan 5V och 1023
   float voltage = sensorValue * (5.0 / 1023.0); 
 
+  // Skriver ut det råa ADC värdet och spänningen till serieporten
   Serial.print("Raw ADC Value: ");
   Serial.print(sensorValue);
   Serial.print("\tVoltage: ");
