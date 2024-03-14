8b3fba3fd13df7a773fd8c5d3ecb7a7fc0830d07
Saran Kongthong <146092374+Sezzry@users.noreply.github.com>
2024-03-14 23:33:19 +0100

    Update uppgift4.ino

--- uppgift4.ino
+++ uppgift4.ino
@@ -1,37 +1,42 @@
-const int buttonPin1 = 13;
-const int buttonPin2 = 7;
-const int potPin = A0;
-const int ledPinR = 11;
-const int ledPinG = 9;
-const int ledPinB = 3;
+const int buttonPin1 = 13;  // Flytta från pin 3 till 13
+const int buttonPin2 = 7;   // Flytta från pin 2 till 7
+const int potPin = A0;      
+const int ledPinR = 11;     
+const int ledPinG = 9;      
+const int ledPinB = 3;      
 
-int buttonState1 = 0;
-int buttonState2 = 0;
-int lastButtonState1 = 0;
-int lastButtonState2 = 0;
-int potValue = 0;
+int buttonState1 = 0;       
+int buttonState2 = 0;       
+int lastButtonState1 = 0;   
+int lastButtonState2 = 0;   
+int potValue = 0;           
 
-unsigned long lastDebounceTime1 = 0;
-unsigned long lastDebounceTime2 = 0;
-unsigned long debounceDelay = 50;
+// Variabler debouncing
+unsigned long lastDebounceTime1 = 0;  // Senaste stabiliseringstidpunkt för knapp 1
+unsigned long lastDebounceTime2 = 0;  // Senaste stabiliseringstidpunkt för knapp 2
+unsigned long debounceDelay = 50;     // Debounce-fördröjningstid
 
 void setup() {
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
-  pinMode(potPin, INPUT);
-  pinMode(ledPinR, OUTPUT);
-  pinMode(ledPinG, OUTPUT);
-  pinMode(ledPinB, OUTPUT);
+  pinMode(buttonPin1, INPUT);  
+  pinMode(buttonPin2, INPUT);  
+  pinMode(potPin, INPUT);      
+  pinMode(ledPinR, OUTPUT);    
+  pinMode(ledPinG, OUTPUT);    
+  pinMode(ledPinB, OUTPUT);    
 
-  Serial.begin(9600);
+  Serial.begin(9600); 
 }
 
 void loop() {
   int reading1 = digitalRead(buttonPin1);
+
+  // Om knappens tillstånd har ändrats
   if (reading1 != lastButtonState1) {
+    // Uppdatera senaste stabiliseringstidpunkt för knapp 1
     lastDebounceTime1 = millis();
   }
 
+  // Om det har gått tillräckligt med tid sedan senaste stabilisering av knapp 1
   if ((millis() - lastDebounceTime1) > debounceDelay) {
     if (reading1 != buttonState1) {
       buttonState1 = reading1;
@@ -42,13 +47,17 @@ void loop() {
   }
 
   int reading2 = digitalRead(buttonPin2);
+
+  // Om knappens tillstånd har ändrats
   if (reading2 != lastButtonState2) {
+    // Uppdatera senaste stabiliseringstidpunkt för knapp 2
     lastDebounceTime2 = millis();
   }
 
   if ((millis() - lastDebounceTime2) > debounceDelay) {
     if (reading2 != buttonState2) {
       buttonState2 = reading2;
+
       if (buttonState2 == LOW) {
         digitalWrite(ledPinG, !digitalRead(ledPinG));
       }
@@ -60,6 +69,7 @@ void loop() {
 
   Serial.print("Potentiometer value (LED-B): ");
   Serial.println(potValue);
+  
   delay(200);
 
   lastButtonState1 = reading1;
