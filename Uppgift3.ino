492235c6a8b311398dcaa36b1b3773b7d358c39c
Saran Kongthong <146092374+Sezzry@users.noreply.github.com>
2024-03-14 23:24:00 +0100

    Update Uppgift3.ino

--- Uppgift3.ino
+++ Uppgift3.ino
@@ -1,32 +1,36 @@
-const int buttonPin1 = 13; 
-const int buttonPin2 = 7;  
-const int potPin = A0;     
-const int ledPinR = 11;    
-const int ledPinG = 9;     
-const int ledPinB = 3;     
+const int buttonPin1 = 3;  // Knapp 1, flytta pin till 13 eller använd pin 3
+const int buttonPin2 = 2;   // Knapp 2, flytta pin till 7 eller använd pin 2
+const int potPin = A0;      // Potentiometer
+const int ledPinR = 11;     // Röd LED
+const int ledPinG = 9;      // Grön LED
+const int ledPinB = 6;      // Blå LED
 
-int buttonState1 = 0;      
-int buttonState2 = 0;      
-int lastButtonState1 = 0;  
-int lastButtonState2 = 0;  
-int potValue = 0;          
+int buttonState1 = 0;       // Knapp 1 aktuellt tillstånd
+int buttonState2 = 0;       // Knapp 2 aktuellt tillstånd
+int lastButtonState1 = 0;   // Knapp  föregående tillstånd
+int lastButtonState2 = 0;   // Knapp 2 föregående tillstånd
+int potValue = 0;           // Potentiometervärde
 
 void setup() {
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
-  pinMode(potPin, INPUT);
-  pinMode(ledPinR, OUTPUT);
-  pinMode(ledPinG, OUTPUT);
-  pinMode(ledPinB, OUTPUT);
+  pinMode(buttonPin1, INPUT);  // Inmatning knapp 1
+  pinMode(buttonPin2, INPUT);  // Inmatning knapp 2
+  pinMode(potPin, INPUT);      // Inmatning potentiometer
+  pinMode(ledPinR, OUTPUT);    // Utgång Röd LED
+  pinMode(ledPinG, OUTPUT);    // Utgång Grön LED
+  pinMode(ledPinB, OUTPUT);    // Utgång Blå LED
 
   Serial.begin(9600); 
 }
 
 void loop() {
+  // Läs knapp 1
   buttonState1 = digitalRead(buttonPin1);
 
+  // Kollar om knappens tillstånd har ändrats
   if (buttonState1 != lastButtonState1) {
+    // Kollar om knappen trycks ned
     if (buttonState1 == LOW) {
+      // Vänd på den röda LED ljuset (tänd om släckt, släck om tänd)
       digitalWrite(ledPinR, !digitalRead(ledPinR));
     }
   }
@@ -40,12 +44,17 @@ void loop() {
   }
 
   potValue = analogRead(potPin);
-  analogWrite(ledPinB, potValue / 4); 
+  // Skicka ut potentiometervärde som PWM-signal till den blå ljuset
+  analogWrite(ledPinB, potValue / 4); // Dela värdet för att anpassa till 0-255
 
+  // Skriv ut potentiometervärdet till serial monitor
   Serial.print("Potentiometer value (LED-B): ");
   Serial.println(potValue);
+  
+  // Fördröjning för att undvika att överbelasta seriell kommunikation, fördröjning kan justeras efter behov
   delay(200);
 
+  // Uppdatera föregående knapptryckningstillstånd
   lastButtonState1 = buttonState1;
   lastButtonState2 = buttonState2;
 }
